d6a8824f48ae0f7d041f721f31fbffed1f13175f
Morariu Dorian Sebastian <dorian_tm@yahoo.com>
2022-08-30 19:01:06 +0300

    Created cars table view

---
+++ DotNetCarsWebsite/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCarsWebsite.Data;
+using DotNetCarsWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCarsWebsite.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly CarsDbContext CarsDb;
+        public CarController(CarsDbContext carsDb)
+        {
+            CarsDb = carsDb;
+        }
+
+        public IActionResult Index()
+        {
+            List<Car> Cars = CarsDb.Cars.ToList();
+            return View(Cars);
+        }
+    }
+}
